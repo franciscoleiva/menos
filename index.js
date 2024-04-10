35a11314b61eb7c41ac0e6039ed77b3ef99f2620
franciscoleiva <52587671+franciscoleiva@users.noreply.github.com>
2024-04-10 19:22:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,17 @@
 const axios = require('axios');
 
-// URL de la API
 const url = process.env.CONSUMER1
-const url2 = process.env.CONSUMER2
 const sl1 = process.env.SOL1
-const sl2 = process.env.SOL2
 
-// Parámetro a enviar en el cuerpo de la petición
 const dataD = {
   solicitud: sl1
 };
 
-// Realizar la petición POST
 axios.post(url, dataD)
   .then(response => {
     console.log('Respuesta:', response.data);
-    obtenerValorUF();
   })
   .catch(error => {
     console.error('Error al realizar la petición:', error);
   });
 
-  async function obtenerValorUF() {
-    try {
-        const response = await axios.get(url2);
-        
-        if (response.status !== 200) {
-            throw new Error('Error al obtener los datos de la API');
-        }
-        const data = response.data;
-        let valorUF = Math.round(data.uf.valor);
-        console.log(valorUF)
-        const dataU = {
-          solicitud: sl2,
-          valor: valorUF
-        };
-
-        axios.post(url, dataU)
-        .then(response => {
-          console.log('Respuesta:', response.data);
-        })
-        .catch(error => {
-          console.error('Error al realizar la petición:', error);
-        });
-        
-        return 
-    } catch (error) {
-        console.error('Error:', error.message);
-        throw error;
-    }
-}
